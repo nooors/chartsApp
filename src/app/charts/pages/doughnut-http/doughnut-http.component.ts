f301bbc8c37d97f41559cc4d88512364e5b46e6c
Jose Nortes <nooors@gmail.com>
2022-07-29 08:40:21 +0200

    ChartApp complete, TODO: deploy db in heroku

--- src/app/charts/pages/doughnut-http/doughnut-http.component.ts
+++ src/app/charts/pages/doughnut-http/doughnut-http.component.ts
@@ -1,16 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ChartData, ChartType } from "chart.js";
+import { ChartsService } from "./../../services/charts.service";
 
 @Component({
-  selector: 'app-doughnut-http',
-  templateUrl: './doughnut-http.component.html',
-  styles: [
-  ]
+  selector: "app-doughnut-http",
+  templateUrl: "./doughnut-http.component.html",
+  styles: [],
 })
 export class DoughnutHttpComponent implements OnInit {
-
-  constructor() { }
+  public barChartTypeDought: ChartType = "doughnut";
+  public barChartDataDought: ChartData<"bar"> = {
+    // labels: ["2006", "2007", "2008", "2009", "2010"],
+    datasets: [
+      {
+        data: [],
+        label: "Series A",
+        backgroundColor: [
+          "#F5D905",
+          "#FC7605",
+          "#4E05FC",
+          "#05B2F2",
+          "#E60759",
+        ],
+        hoverBackgroundColor: ["#F5B80A"],
+      },
+    ],
+  };
+  constructor(private chartSrv: ChartsService) {}
 
   ngOnInit(): void {
-  }
+    // Here manipulate data once we have received it
 
+    // this.chartSrv.getUsersSocialMedia().subscribe((resp) => {
+    //   console.log(resp);
+    //   const labels = Object.keys(resp);
+    //   const values = Object.values(resp);
+    //   this.barChartDataDought.datasets[0].data = values;
+    //   this.barChartDataDought.labels = labels;
+    // });
+
+    // Here we receive data in the fornat we want from the service. In this case we have an object with two arrays that we can destructure
+    this.chartSrv
+      .getUsersSocialMediaDoughnutData()
+      .subscribe(({ values, labels }) => {
+        this.barChartDataDought.labels = labels;
+        this.barChartDataDought.datasets[0].data = values;
+      });
+  }
 }
--- src/app/charts/services/charts.service.ts
+++ src/app/charts/services/charts.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { delay, map } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ChartsService {
+  constructor(private httpSrv: HttpClient) {}
 
-  constructor() { }
+  getUsersSocialMedia() {
+    return this.httpSrv.get("http://localhost:3000/chart");
+  }
+
+  // with RxJS return the data in the formant we want.
+  getUsersSocialMediaDoughnutData() {
+    return this.httpSrv.get("http://localhost:3000/chart").pipe(
+      delay(1000),
+      map((resp) => {
+        const values = Object.values(resp);
+        const labels = Object.keys(resp);
+        return { values, labels };
+      }),
+    );
+  }
 }
